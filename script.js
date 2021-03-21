bbcc2fbaa311f7ae4701b8e5af0b2c0237c2b701
Jatin Jain <sj3521088@gmail.com>
2021-03-21 14:33:37 +0530

    interior

---
+++ script.js
@@ -0,0 +1,17 @@
+let t1 = gsap.timeline();
+
+// t1.from("#nav", {
+//     stagger: .4,
+//     duration: 3,
+//     opacity: 0,
+//     x: 30,
+//     ease: "Expo.easeInOut",
+// })
+// t1.from("midleftcontent h3", {
+//     stagger: .4,
+//     duration: 3,
+//     opacity: 0,
+//     y: 30,
+//     ease: "Expo.easeInOut",
+// })
+
